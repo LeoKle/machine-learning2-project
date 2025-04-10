c5eff1a87db9a4e9b09dbbffcab74eae86aaf36b
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-04-10 09:49:49 +0200

    add static Metrics class

---
+++ src/classes/metrics.py
@@ -0,0 +1,51 @@
+class Metrics:
+    @staticmethod
+    def accuracy(tp, tn, fp, fn):
+        return (tp + tn) / (tp + tn + fp + fn) if (tp + tn + fp + fn) != 0 else 0.0
+
+    @staticmethod
+    def error_rate(tp, tn, fp, fn):
+        return (fp + fn) / (tp + tn + fp + fn) if (tp + tn + fp + fn) != 0 else 0.0
+
+    @staticmethod
+    def precision(tp, tn, fp, fn):
+        return tp / (tp + fp) if (tp + fp) != 0 else 0.0
+
+    @staticmethod
+    def recall(tp, tn, fp, fn):  # Sensitivity
+        return tp / (tp + fn) if (tp + fn) != 0 else 0.0
+
+    @staticmethod
+    def specificity(tp, tn, fp, fn):
+        return tn / (tn + fp) if (tn + fp) != 0 else 0.0
+
+    @staticmethod
+    def negative_predictive_value(tp, tn, fp, fn):
+        return tn / (tn + fn) if (tn + fn) != 0 else 0.0
+
+    @staticmethod
+    def false_positive_rate(tp, tn, fp, fn):
+        return fp / (fp + tn) if (fp + tn) != 0 else 0.0
+
+    @staticmethod
+    def false_negative_rate(tp, tn, fp, fn):
+        return fn / (fn + tp) if (fn + tp) != 0 else 0.0
+
+    @staticmethod
+    def f1_score(tp, tn, fp, fn):
+        precision = Metrics.precision(tp, tn, fp, fn)
+        recall = Metrics.recall(tp, tn, fp, fn)
+        return (
+            2 * precision * recall / (precision + recall)
+            if (precision + recall) != 0
+            else 0.0
+        )
+
+    @staticmethod
+    def fbeta_score(tp, tn, fp, fn, beta=1.0):
+        precision = Metrics.precision(tp, tn, fp, fn)
+        recall = Metrics.recall(tp, tn, fp, fn)
+        beta_squared = beta**2
+        numerator = (1 + beta_squared) * precision * recall
+        denominator = beta_squared * precision + recall
+        return numerator / denominator if denominator != 0 else 0.0
---
+++ src/tests/classes/test_metrics.py
@@ -0,0 +1,89 @@
+import unittest
+from math import isclose
+
+from classes.metrics import Metrics
+
+
+class TestMetrics(unittest.TestCase):
+    def setUp(self):
+        self.tp = 50
+        self.tn = 30
+        self.fp = 10
+        self.fn = 10
+
+    def test_accuracy(self):
+        result = Metrics.accuracy(self.tp, self.tn, self.fp, self.fn)
+        expected = (self.tp + self.tn) / (self.tp + self.tn + self.fp + self.fn)
+        self.assertTrue(isclose(result, expected))
+
+    def test_error_rate(self):
+        result = Metrics.error_rate(self.tp, self.tn, self.fp, self.fn)
+        expected = (self.fp + self.fn) / (self.tp + self.tn + self.fp + self.fn)
+        self.assertTrue(isclose(result, expected))
+
+    def test_precision(self):
+        result = Metrics.precision(self.tp, self.tn, self.fp, self.fn)
+        expected = self.tp / (self.tp + self.fp)
+        self.assertTrue(isclose(result, expected))
+
+    def test_recall(self):
+        result = Metrics.recall(self.tp, self.tn, self.fp, self.fn)
+        expected = self.tp / (self.tp + self.fn)
+        self.assertTrue(isclose(result, expected))
+
+    def test_specificity(self):
+        result = Metrics.specificity(self.tp, self.tn, self.fp, self.fn)
+        expected = self.tn / (self.tn + self.fp)
+        self.assertTrue(isclose(result, expected))
+
+    def test_negative_predictive_value(self):
+        result = Metrics.negative_predictive_value(self.tp, self.tn, self.fp, self.fn)
+        expected = self.tn / (self.tn + self.fn)
+        self.assertTrue(isclose(result, expected))
+
+    def test_false_positive_rate(self):
+        result = Metrics.false_positive_rate(self.tp, self.tn, self.fp, self.fn)
+        expected = self.fp / (self.fp + self.tn)
+        self.assertTrue(isclose(result, expected))
+
+    def test_false_negative_rate(self):
+        result = Metrics.false_negative_rate(self.tp, self.tn, self.fp, self.fn)
+        expected = self.fn / (self.fn + self.tp)
+        self.assertTrue(isclose(result, expected))
+
+    def test_f1_score(self):
+        precision = self.tp / (self.tp + self.fp)
+        recall = self.tp / (self.tp + self.fn)
+        expected = 2 * precision * recall / (precision + recall)
+        result = Metrics.f1_score(self.tp, self.tn, self.fp, self.fn)
+        self.assertTrue(isclose(result, expected))
+
+    def test_fbeta_score(self):
+        beta = 2
+        precision = self.tp / (self.tp + self.fp)
+        recall = self.tp / (self.tp + self.fn)
+        beta_squared = beta**2
+        expected = (
+            (1 + beta_squared)
+            * precision
+            * recall
+            / (beta_squared * precision + recall)
+        )
+        result = Metrics.fbeta_score(self.tp, self.tn, self.fp, self.fn, beta=beta)
+        self.assertTrue(isclose(result, expected))
+
+    def test_zero_divisions(self):
+        self.assertEqual(Metrics.accuracy(0, 0, 0, 0), 0.0)
+        self.assertEqual(Metrics.error_rate(0, 0, 0, 0), 0.0)
+        self.assertEqual(Metrics.precision(0, 0, 0, 0), 0.0)
+        self.assertEqual(Metrics.recall(0, 0, 0, 0), 0.0)
+        self.assertEqual(Metrics.specificity(0, 0, 0, 0), 0.0)
+        self.assertEqual(Metrics.negative_predictive_value(0, 0, 0, 0), 0.0)
+        self.assertEqual(Metrics.false_positive_rate(0, 0, 0, 0), 0.0)
+        self.assertEqual(Metrics.false_negative_rate(0, 0, 0, 0), 0.0)
+        self.assertEqual(Metrics.f1_score(0, 0, 0, 0), 0.0)
+        self.assertEqual(Metrics.fbeta_score(0, 0, 0, 0, beta=2), 0.0)
+
+
+if __name__ == "__main__":
+    unittest.main()
