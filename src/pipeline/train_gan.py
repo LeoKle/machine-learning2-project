2f5b1fce92429591e93970904d9496c84d6dc30d
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-04-22 12:30:01 +0200

    update train_gan

--- src/pipeline/train_gan.py
+++ src/pipeline/train_gan.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 
 from utils.device import DEVICE
+from utils.plotter import Plotter
 
 
 class GanTrainingPipeline:
@@ -34,13 +35,15 @@ class GanTrainingPipeline:
         batch = batch.to(DEVICE)
 
         # train discriminator on real data
-        label_real = torch.ones(batch_size, 1, device=DEVICE)
+        label_real = torch.ones(batch_size, 1, device=DEVICE) * 0.9
 
-        output_real = self.discriminator(batch)
+        noise_image = batch  # + torch.randn_like(batch) * 0.05
+        output_real = self.discriminator(noise_image)
+
         loss_real = self.loss_function_discriminator(output_real, label_real)
 
         # train discriminator on generated data
-        label_fake = torch.zeros(batch_size, 1, device=DEVICE)
+        label_fake = torch.zeros(batch_size, 1, device=DEVICE)  # * 0.1
         latent_tensor = torch.randn(
             batch_size, self.generator.latent_dim, device=DEVICE
         )
@@ -50,11 +53,13 @@ class GanTrainingPipeline:
         loss_generated = self.loss_function_discriminator(output_generated, label_fake)
 
         total_loss = loss_real + loss_generated
+        self.discriminator_loss.append(total_loss)
+
         total_loss.backward()
         self.optimizer_discriminator.step()
 
     def train_generator(self, batch: torch.Tensor):
-        self.discriminator.zero_grad()
+        self.generator.zero_grad()
         batch_size = batch.shape[0]
 
         latent_tensor = torch.randn(batch_size, self.generator.latent_dim).to(DEVICE)
@@ -64,19 +69,68 @@ class GanTrainingPipeline:
         discriminator_output = self.discriminator(generator_output)
 
         loss = self.loss_function_generator(discriminator_output, label_real)
+        self.generator_loss.append(loss)
 
         loss.backward()
         self.optimizer_generator.step()
 
     def train(self, epoch):
         for epoch in range(1, epoch + 1):
+            print("Training epoch", epoch)
             self.generator.train()
             self.discriminator.train()
 
-            for batch_idx, (x, _) in enumerate(self.dataloader_train):
+            self.generator_loss = []
+            self.discriminator_loss = []
+
+            for _, (x, _) in enumerate(self.dataloader_train):
                 self.train_generator(x)
                 self.train_discriminator(x)
+
+            generator_loss = torch.tensor(self.generator_loss).mean()
+            discriminator_loss = torch.tensor(self.discriminator_loss).mean()
+
+            print(f"Generator Loss: {generator_loss}")
+            print(f"Discriminator Loss: {discriminator_loss}")
 
             with torch.no_grad():
                 self.generator.eval()
                 self.discriminator.eval()
+
+                real_preds = []
+                real_targets = []
+
+                fake_preds = []
+                fake_targets = []
+
+                for _, (x, y) in enumerate(self.dataloader_test):
+                    x = x.to(DEVICE)
+                    real_out = self.discriminator(x)
+                    real_preds.extend((real_out > 0.5).float().cpu())
+                    real_targets.extend(torch.ones_like(real_out).cpu())
+
+                    z = torch.randn(x.size(0), self.generator.latent_dim, device=DEVICE)
+                    fake_imgs = self.generator(z)
+                    fake_out = self.discriminator(fake_imgs)
+                    fake_preds.extend((fake_out > 0.5).float().cpu())
+                    fake_targets.extend(torch.zeros_like(fake_out).cpu())
+
+                preds = torch.cat(real_preds + fake_preds)
+                targets = torch.cat(real_targets + fake_targets)
+
+                accuracy = (preds == targets).float().mean().item()
+                print(f"Discriminator accuracy on test set: {accuracy:.4f}")
+
+                image_count = 1
+                latent_tensor = torch.randn(image_count, self.generator.latent_dim).to(
+                    DEVICE
+                )
+
+                generator_output = self.generator(latent_tensor)
+
+                Plotter.plot(
+                    generator_output[0], output_file_name=f"output/gan_{epoch}.png"
+                )
+                Plotter.plot(
+                    generator_output[0], output_file_name=f"output/#latest.png"
+                )
--- src/tests/pipeline/test_train_gan.py
+++ src/tests/pipeline/test_train_gan.py
@@ -2,7 +2,7 @@ import unittest
 import torch
 import torch.nn as nn
 
-from data.mnist import get_dataloaders
+from data.mnist import get_mnist_dataloaders
 from models.gan.discriminator import Discriminator
 from models.gan.generator import Generator
 from pipeline.train_gan import GanTrainingPipeline
@@ -17,8 +17,12 @@ class TestGanPipeline(unittest.TestCase):
         cls.loss_function_discriminator = nn.BCELoss()
         cls.loss_function_generator = nn.BCELoss()
 
-        cls.optimizer_discriminator = torch.optim.Adam(cls.discriminator.parameters())
-        cls.optimizer_generator = torch.optim.Adam(cls.generator.parameters())
+        cls.optimizer_discriminator = torch.optim.Adam(
+            cls.discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999)
+        )
+        cls.optimizer_generator = torch.optim.Adam(
+            cls.generator.parameters(), lr=0.0002, betas=(0.5, 0.999)
+        )
 
         cls.dataloader_train, cls.dataloader_test = get_mnist_dataloaders()
 
