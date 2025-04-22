c752e4c44e79b203f1f9c0bcbd29a1db7dd70a51
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-04-22 11:23:47 +0200

    refactor dataloaders

--- src/data/cifar10.py
+++ src/data/cifar10.py
@@ -1,47 +1,29 @@
-import torch
-import torchvision
-from torchvision import transforms
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
 from data.seed import generator
 from utils.root_folder import find_project_root
 
-_ROOT_FILE = find_project_root() / "data"
 
-transform = transforms.Compose(
-    [
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ]
-)
+def get_cifar10_dataloaders(batch_size=64, transform=None, root=None, download=True):
+    transform = transform or transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ]
+    )
+    root = root or (find_project_root() / "data")
 
-cifar10_dataset_train = torchvision.datasets.CIFAR10(
-    root=_ROOT_FILE,
-    train=True,
-    transform=transform,
-    download=True,
-)
+    train_dataset = datasets.CIFAR10(
+        root=root, train=True, transform=transform, download=download
+    )
+    test_dataset = datasets.CIFAR10(root=root, train=False, transform=transform)
 
-cifar10_dataset_test = torchvision.datasets.CIFAR10(
-    root=_ROOT_FILE,
-    train=False,
-    transform=transform,
-    download=True,
-)
-
-
-def get_dataloaders(batch_size=64):
-    cifar10_dataloader_train = torch.utils.data.DataLoader(
-        dataset=cifar10_dataset_train,
-        batch_size=batch_size,
-        shuffle=True,
-        generator=generator,
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, generator=generator
+    )
+    test_loader = DataLoader(
+        test_dataset, batch_size=batch_size, shuffle=False, generator=generator
     )
 
-    cifar10_dataloader_test = torch.utils.data.DataLoader(
-        dataset=cifar10_dataset_test,
-        batch_size=batch_size,
-        shuffle=False,
-        generator=generator,
-    )
-
-    return cifar10_dataloader_train, cifar10_dataloader_test
+    return train_loader, test_loader
--- src/data/mnist.py
+++ src/data/mnist.py
@@ -1,43 +1,26 @@
-import torch
-import torchvision
-from torchvision import transforms
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
 from data.seed import generator
 from utils.root_folder import find_project_root
 
-_ROOT_FILE = find_project_root() / "data"
 
-mnist_dataset_train = torchvision.datasets.MNIST(
-    root=_ROOT_FILE,
-    train=True,
-    transform=transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.01307), (0.3081))]
-    ),
-    download=True,
-)
+def get_mnist_dataloaders(batch_size=64, transform=None, root=None, download=True):
+    transform = transform or transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+    )
+    root = root or (find_project_root() / "data")
 
-mnist_dataset_test = torchvision.datasets.MNIST(
-    root=_ROOT_FILE,
-    train=False,
-    transform=transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.01307), (0.3081))]
-    ),
-)
+    train_dataset = datasets.MNIST(
+        root=root, train=True, transform=transform, download=download
+    )
+    test_dataset = datasets.MNIST(root=root, train=False, transform=transform)
 
-
-def get_dataloaders(batch_size=64):
-    mnist_dataloader_train = torch.utils.data.DataLoader(
-        dataset=mnist_dataset_train,
-        batch_size=batch_size,
-        shuffle=True,
-        generator=generator,
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, generator=generator
+    )
+    test_loader = DataLoader(
+        test_dataset, batch_size=batch_size, shuffle=False, generator=generator
     )
 
-    mnist_dataloader_test = torch.utils.data.DataLoader(
-        dataset=mnist_dataset_test,
-        batch_size=batch_size,
-        shuffle=False,
-        generator=generator,
-    )
-
-    return mnist_dataloader_train, mnist_dataloader_test
+    return train_loader, test_loader
