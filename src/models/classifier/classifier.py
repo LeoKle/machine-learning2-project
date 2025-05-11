83e990aa5cdfd9a39b2ad65eb75b91aed5c7be43
xSunny777 <hetian.zhang@study.hs-duesseldorf.de>
2025-05-11 16:51:52 +0200

    add classifier and test_classifier

---
+++ src/models/classifier/classifier.py
@@ -0,0 +1,28 @@
+from typing import Literal
+import torch.nn as nn
+
+class Classifier(nn.Module):
+    def __init__(self, dataset: Literal["MNIST", "CIFAR10"] = "MNIST"):
+        super().__init__()
+        
+        if dataset == "MNIST":
+            input_size = 1 * 28 * 28 
+            hidden_size = 512 # zu groß??
+        elif dataset == "CIFAR10":
+            input_size = 3 * 32 * 32
+            hidden_size = 1024  # zu groß??
+        else:
+            raise ValueError("Unsupported dataset: choose 'MNIST' or 'CIFAR10'")
+        
+        self.main = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(True),
+            nn.Linear(hidden_size, hidden_size // 2),
+            nn.ReLU(True),
+            nn.Linear(hidden_size // 2, 10),
+            nn.LogSoftmax(dim=1)
+        )
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1) 
+        return self.main(x)
---
+++ src/tests/models/classifier/test_classifier.py
@@ -0,0 +1,24 @@
+import unittest
+import torch
+
+from models.classifier.classifier import Classifier
+
+
+class TestClassifier(unittest.TestCase):
+    def test_classifier_mnist(self):
+        model = Classifier(dataset="MNIST")
+        dummy_input = torch.randn(4, 1, 28, 28)  # batch_size, channels, height, width
+        output = model(dummy_input)
+
+        self.assertEqual(output.shape, (4, 10))  # batch_size, num_classes
+
+    def test_classifier_cifar10(self):
+        model = Classifier(dataset="CIFAR10")
+        dummy_input = torch.randn(4, 3, 32, 32) 
+        output = model(dummy_input)
+
+        self.assertEqual(output.shape, (4, 10))  
+
+
+if __name__ == "__main__":
+    unittest.main()
