0df403cfd6b014218bd2c72ae504d99252ebaa2b
GitHub Action <action@github.com>
2025-06-01 18:26:53 +0000

    Format code with black

--- src/tests/pipeline/test_train_classifier.py
+++ src/tests/pipeline/test_train_classifier.py
@@ -8,11 +8,12 @@ from data.mnist import get_mnist_dataloaders
 from models.classifier.classifier import Classifier
 from pipeline.train_classifier import ClassifierTrainingPipeline
 
+
 class TestClassifierPipeline(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.dataloader_train, cls.dataloader_test = get_mnist_dataloaders(batch_size=32)
-        
+
         cls.model = Classifier(dataset="MNIST")
         cls.loss_function = nn.NLLLoss()
         cls.optimizer = optim.Adam(cls.model.parameters(), lr=0.001)
@@ -30,25 +31,26 @@ class TestClassifierPipeline(unittest.TestCase):
         self.assertIsInstance(self.pipeline.model, nn.Module)
         self.assertIsInstance(self.pipeline.loss_function, nn.Module)
         self.assertIsInstance(self.pipeline.optimizer, optim.Optimizer)
-        
+
     def test_train_epoch(self):
         for batch, labels in self.dataloader_train:
             self.pipeline.train_epoch(batch, labels)
-            break 
-        
+            break
+
     def test_evaluation(self):
         self.pipeline.current_epoch = 1
         loss, accuracy = self.pipeline.evaluate()
         self.assertIsInstance(loss, float)
         self.assertIsInstance(accuracy, float)
         self.assertTrue(0 <= accuracy <= 100)
-        
+
     def test_full_training(self):
         test_save_path = Path("test_model.pth")
-        
+
         self.pipeline.train(epoch=1, save_path=test_save_path)
         self.assertTrue(test_save_path.exists())
-        test_save_path.unlink() 
+        test_save_path.unlink()
+
 
 if __name__ == "__main__":
     unittest.main()
