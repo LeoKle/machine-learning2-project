1f80ac30685e2383abf4f92627b0de9c64f1f9ad
xSunny777 <hetian.zhang@study.hs-duesseldorf.de>
2025-06-11 01:20:22 +0200

    updated more complex layer for classifier

--- src/models/classifier/classifier_linear.py
+++ src/models/classifier/classifier_linear.py
@@ -4,10 +4,21 @@ class Classifier(nn.Module):
     def __init__(self, input_size: int, num_classes: int = 10):
         super().__init__()
         self.main = nn.Sequential(
-            nn.Linear(input_size, 128),
-            nn.ReLU(True),
+            nn.Linear(input_size, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.4),
+
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(0.1),
             nn.Dropout(0.3),
-            nn.Linear(128, num_classes),
+
+            nn.Linear(128, 64),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.2),
+
+            nn.Linear(64, num_classes),
             nn.LogSoftmax(dim=1)
         )
 
