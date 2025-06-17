3d86d9f0dadcdb666a0c337ac7c59a88caad52c0
xSunny777 <hetian.zhang@study.hs-duesseldorf.de>
2025-06-17 03:34:51 +0200

    clearer structure in encoder_classifier.py

--- src/models/classifier/encoder_classifier.py
+++ src/models/classifier/encoder_classifier.py
@@ -1,25 +1,21 @@
 import torch.nn as nn
 import torch
-from models.classifier.classifier_linear import Classifier, ClassifierDeep
-from models.classifier.classifier_resnet import ClassifierResNet, ClassifierLinear
+from models.classifier.classifier_linear import ClassifierMLP, ClassifierMLPDeep
+from models.classifier.classifier_resnet import ClassifierResNet, ClassifierLarge
 
 class EncoderClassifier(nn.Module):
     def __init__(self, encoder, classifier=None, num_classes=10, img_channels = 1, img_size = 28, fine_tune_encoder=True):
         super().__init__()
         self.encoder = encoder
+        self.img_channels = img_channels
+        self.img_size = img_size
 
         if not fine_tune_encoder:
             for param in self.encoder.parameters():
                 param.requires_grad = False
 
-        self.img_channels = img_channels
-        self.img_size = img_size
         self.encoder_output_size = self.get_encoder_output_size()
-
-        if classifier is None:
-            self.classifier = ClassifierResNet(input_size=self.encoder_output_size, num_classes=num_classes)
-        else:
-            self.classifier = classifier
+        self.classifier = classifier
 
     def get_encoder_output_size(self):
         with torch.no_grad():
