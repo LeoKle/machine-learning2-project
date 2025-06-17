aabf548566e494ebe9dd9b721f727adc07a857d9
xSunny777 <hetian.zhang@study.hs-duesseldorf.de>
2025-06-17 03:31:25 +0200

    added utils.data_setup.py in order to automatically decide the datatype

---
+++ src/utils/data_setup.py
@@ -0,0 +1,17 @@
+import torch
+from data.mnist import get_mnist_dataloaders
+from data.cifar10 import get_cifar10_dataloaders
+
+def prepare_dataset(dataset_type: str, batch_size: int):
+    if dataset_type == "MNIST":
+        train_loader, test_loader = get_mnist_dataloaders(batch_size=batch_size)
+        dummy_input = torch.randn(1, 1, 28, 28)
+        encoder_path = "resultsAECNN2_MNIST/MNIST_encoder_weights.pth"
+    elif dataset_type == "CIFAR10":
+        train_loader, test_loader = get_cifar10_dataloaders(batch_size=batch_size)
+        dummy_input = torch.randn(1, 3, 32, 32)
+        encoder_path = "resultsAECNN2_CIFAR10/CIFAR10_encoder_weights.pth"
+    else:
+        raise ValueError("Unsupported dataset_type")
+
+    return train_loader, test_loader, dummy_input, encoder_path
