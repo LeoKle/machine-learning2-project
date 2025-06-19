5c9a9064e91d5bcff95fdeed2d269f1e484653e9
xSunny777 <hetian.zhang@study.hs-duesseldorf.de>
2025-06-19 23:18:05 +0200

    added cnn classifier model

--- src/utils/data_setup.py
+++ src/utils/data_setup.py
@@ -15,3 +15,14 @@ def prepare_dataset(dataset_type: str, batch_size: int):
         raise ValueError("Unsupported dataset_type")
 
     return train_loader, test_loader, dummy_input, encoder_path
+
+def prepare_dataset_for_cnn(dataset_type: str, batch_size: int):
+    if dataset_type == "MNIST":
+        train_loader, test_loader = get_mnist_dataloaders(batch_size=batch_size)
+        dummy_input = torch.randn(1, 1, 28, 28)
+    elif dataset_type == "CIFAR10":
+        train_loader, test_loader = get_cifar10_dataloaders(batch_size=batch_size)
+        dummy_input = torch.randn(1, 3, 32, 32)
+    else:
+        raise ValueError("Unsupported dataset_type")
+    return train_loader, test_loader, dummy_input
